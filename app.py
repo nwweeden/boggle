4390470ec87f81334ddf9da261eaf496bef22164
Nick Weeden <nicholas_weeden@alumni.brown.edu>
2020-07-24 16:04:17 -0700

    set up route for submitted words

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, jsonify
 from uuid import uuid4
+from flask_debugtoolbar import DebugToolbarExtension
 
 from boggle import BoggleWordList, BoggleBoard
 
@@ -7,6 +8,8 @@ SESS_BOARD_UUID_KEY = "board_uuid"
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "this-is-secret"
+
+debug = DebugToolbarExtension(app)
 
 word_list = BoggleWordList()
 
@@ -31,3 +34,20 @@ def homepage():
     return render_template(
         "index.html",
         board=board)
+
+@app.route('/api/score-word', methods=['POST'])
+def validate_word():
+    # breakpoint()
+    current_word = request.json['submit_word'] #what is a JSON bdoy??
+    board = boards[session[SESS_BOARD_UUID_KEY]]
+
+    check_word = word_list.check_word(current_word)
+    check_on_board = board.check_word_on_board(current_word)
+
+    if not check_word: 
+        return jsonify({'result': "not-word"})
+    if not check_on_board:
+        return jsonify({'result': "not-on-board"})
+    else:
+        return jsonify({'result': "ok", 'word': f"{current_word}"})
+
